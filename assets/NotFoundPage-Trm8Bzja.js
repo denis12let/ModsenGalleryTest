fcf521c1bdd3c2ef863066d393809f678681d435
denis12let <84den84sokol@gmail.com>
2025-03-17 22:21:09 +0300

    Updates

--- assets/NotFoundPage-Trm8Bzja.js
+++ assets/NotFoundPage-Trm8Bzja.js
@@ -1,4 +1,4 @@
-import{d as e,j as t,T as n}from"./index-glRGlrKp.js";const i=e.div`
+import{d as e,j as t,T as n}from"./index-BS9VaUGl.js";const i=e.div`
   display: flex;
   flex-direction: column;
   align-items: center;
